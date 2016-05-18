6ebb2d16b2c4e0d39b2fbb1c3f8636e2d8aeed1c
Michael Kevin Yang <michael.kevin@gmail.com>
2016-05-18 23:03:55 +0800

    Fixed advanced command and throws an error if there is more than one match for title/code

--- rubybot/rubybot.py
+++ rubybot/rubybot.py
@@ -2,10 +2,19 @@ import asyncio
 import discord
 import textparse
 import random
+import texttable
 
 from discord.ext import commands
 
 info = 'Ruby Bot, your one-stop solution for music queueing! (Now updated with commands.ext)'
+excess_results = 'Your search returned too many results!'
+com_del_delay = 3
+post_del_delay = 1
+ann_del_delay = 30
+
+tbl_limit = 10
+
+codeblock = '```'
 
 class Restricted:
 
@@ -17,40 +26,84 @@ class Restricted:
     async def title(self, ctx, *, title:str):
         output = textparse.fix_input(title)
         data = textparse.title(output)
-        await self.playsong(data[0][4])
-        await asyncio.sleep(10)
-        await self.bot.delete_message(ctx.message)
+        await self.process(ctx, data)
+        
 
     @commands.command(pass_context = True, no_pm = True)
     async def code(self, ctx, *, code:str):
         output = textparse.fix_input(code)
         data = textparse.code(output)
-        await self.playsong(data[0][4])
-        await asyncio.sleep(10)
-        await self.bot.delete_message(ctx.message)
+        await self.process(ctx, data)
 
     @commands.command(pass_context = True, no_pm = True)
     async def album(self, ctx, *, album:str):
         output = textparse.fix_input(album)
         data = textparse.album(output)
-        await self.playlist(self.getlinks(data))
-        await asyncio.sleep(10)
-        await self.bot.delete_message(ctx.message)
+        await self.process(ctx, data, len(data))
 
     @commands.command(pass_context = True, no_pm = True)
     async def adv(self, ctx, *, adv:str):
         output = textparse.fix_input(adv)
-        await self.bot.say(str(output))
-        await asyncio.sleep(10)
+        data = textparse.adv(output)
+        await self.playlist(self.getlinks(data))
+        await asyncio.sleep(com_del_delay)
         await self.bot.delete_message(ctx.message)
 
-    def getlinks(self, fulllist):
+    def get_links(self, fulllist):
         links = list()
 
         for row in fulllist:
             links.append(row[4])
 
         return links
+
+    def strip_useless(self, fulllist):
+        info = list()
+
+        for row in fulllist:
+            info.append(row[:4])
+
+        return info
+
+    async def process(self, ctx, data, max_length = 1):
+        if len(data) <= max_length:
+            await self.playlist(self.get_links(data))
+            await asyncio.sleep(com_del_delay)
+        else:
+            del_later = list()
+            error = codeblock + excess_results
+            error += '\n'
+            if len(data) <= tbl_limit:
+                rows = self.strip_useless(data)
+                titles = ('Song ID', 'Song Title', 'Song Artist', 'Song Album')
+                table = [titles]
+
+                for row in rows:
+                    table.append(row)
+                
+                output = texttable.print_table(table, 1997 - len(error))
+                for x in range(0, len(output)):
+                    if x == 0:
+                        error += output[x]
+                        error += codeblock
+                        msg = await self.bot.say(error)
+                        del_later.append(msg)
+                        
+                    else:
+                        error = codeblock + output[x] + codeblock
+                        msg = await self.bot.say(error)
+                        del_later.append(msg)
+                        
+                await asyncio.sleep(ann_del_delay)
+                for msg in del_later:
+                    await self.bot.delete_message(msg)
+            else:
+                error += codeblock
+                msg = await self.bot.say(error)
+                await asyncio.sleep(ann_del_delay)
+                await self.bot.delete_message(msg)
+        
+        await self.bot.delete_message(ctx.message)
     
     async def playlist(self, linklist):
         for link in linklist:
@@ -58,7 +111,7 @@ class Restricted:
     
     async def playsong(self, link):
         msg = await self.bot.say(self.song_play + ' ' + link)
-        await asyncio.sleep(2)
+        await asyncio.sleep(post_del_delay)
         await self.bot.delete_message(msg)
 
 class Qaz:
--- rubybot/textparse.py
+++ rubybot/textparse.py
@@ -14,8 +14,58 @@ def album(data):
     return music.album(data[0])
 
 def adv(data):
+    print(data)
+    fdata = dict()
+    saved_flag = ''
+    count = 1
+    flag_random = 0
+    flag_repeat = 0
     for x in range(0, len(data)):
-        pass
+        if x == 0 and data[x] not in args:
+            fdata[dbconn.columns[2]] = data[0]
+
+        elif data[x] in args:
+            if args.index(data[x]) >= 8:
+                if data[x] == args[8]:
+                    count = int(data[x])
+                elif data[x] == args[9]:
+                    flag_random = 1
+                    flag_repeat = 1
+                elif data[x] == args[10]:
+                    flag_random = 1
+
+            else:
+                saved_flag = data[x]
+            print(saved_flag)
+
+        else:
+            if saved_flag != '':
+                fdata[dbconn.args[args.index(saved_flag)]] = data[x]
+                saved_flag = ''
+    print(fdata)
+    output = music.advanced(**fdata)
+
+    if flag_random:
+        output = randomize(output, flag_repeat, count)
+    return output
+
+def randomize(rlist, repeat = 0, count = 1):
+    fpick = rlist[:]
+    play = list()
+
+    if count == -1:
+        count = len(fpick)
+
+    for x in range(0, count):
+        if repeat:
+            play.append(fpick[random.randrange(len(fpick))])
+        else:
+            if len(fpick > 0):
+                play.append(fpick.pop(random.randrange(len(fpick))))
+            else:
+                break
+
+    return play
 
 def fix_input(raw):
     text = raw.split()
