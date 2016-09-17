bfafb85c02d7c3d84834484dc5293f6e530a3ea5
lakepower <lakepowered@gmail.com>
2016-09-16 22:34:47 -0400

    clean install script

--- rubybot/install.sh
+++ rubybot/install.sh
@@ -12,7 +12,6 @@ pip3 install bs4
 wget http://downloads.xiph.org/releases/opus/opus-1.1.3.tar.gz
 tar zxfv opus-1.1.3.tar.gz
 cd opus-1.1.3
-ls
 ./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/opus-1.1.3
 make
 sudo make install
