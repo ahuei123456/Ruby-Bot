2f3593f5e7ded125631d0aa161bede2d2d28334d
lakepower <lakepowered@gmail.com>
2016-09-16 22:22:55 -0400

    include server install script

---
+++ rubybot/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Dependencies
+pip3 install async
+pip3 install discord.py
+pip3 install tweepy
+pip3 install bs4
+
+# Install Opus Dependency
+# http://www.linuxfromscratch.org/blfs/view/svn/multimedia/opus.html
+
+wget http://downloads.xiph.org/releases/opus/opus-1.1.3.tar.gz
+tar zxfv opus-1.1.3.tar.gz
+cd opus-1.1.3
+ls
+./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/opus-1.1.3
+make
+sudo make install
