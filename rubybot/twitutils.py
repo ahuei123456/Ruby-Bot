ae169268cbf17ddc639e4e17ada12aecf411563b
DESKTOP-J24ERBK\ahuei123456 <michael.kevin@gmail.com>
2017-04-26 05:33:19 +0800

    Emergency fix

--- rubybot/twitutils.py
+++ rubybot/twitutils.py
@@ -100,6 +100,7 @@ def get_video(status):
 
 def get_images(status):
     links = []
+    medias = []
     try:
         i_status = status.extended_tweet
         medias = i_status['extended_entities']['media']
